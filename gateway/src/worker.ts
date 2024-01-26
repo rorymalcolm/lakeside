3bb1bdc32c0ca4707b965b834d4758bf8890e91b
Rory Malcolm <rorymckenziemalcolm@gmail.com>
2024-01-26 16:56:00 +0000

    refmt v2

--- gateway/src/worker.ts
+++ gateway/src/worker.ts
@@ -10,16 +10,22 @@ type AppendBody = {
 
 export default {
   async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-    if (request.method === 'PUT') {
-      const json = await request.json<AppendBody>();
-      const currentPrefix = `data/order_ts_hour=${new Date().toISOString().slice(0, 13)}`;
-      const putOperation = await env.LAKESIDE_BUCKET.put(`${currentPrefix}/${v4()}.json`, JSON.stringify(json));
-      if (putOperation) {
-        return new Response('OK');
-      } else {
-        return new Response('FAILED', { status: 500 });
+    try {
+      if (request.method === 'PUT') {
+        const json = await request.json<AppendBody>();
+
+        const currentPrefix = `data/order_ts_hour=${new Date().toISOString().slice(0, 13)}`;
+        const putOperation = await env.LAKESIDE_BUCKET.put(`${currentPrefix}/${v4()}.json`, JSON.stringify(json));
+
+        if (putOperation) {
+          return new Response('OK');
+        } else {
+          return new Response('FAILED', { status: 500 });
+        }
       }
+      return new Response('', { status: 405 });
+    } catch (e) {
+      return new Response('failed to process request', { status: 500 });
     }
-    return new Response();
   },
 };
