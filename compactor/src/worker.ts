27c86a5db1c47a551aed89b2c09e9d4b2142ffce
Rory Malcolm <rorymckenziemalcolm@gmail.com>
2024-01-27 10:11:54 +0000

    wasm-pack

---
+++ compactor/src/worker.ts
@@ -0,0 +1,56 @@
+import { ParquetSchema } from 'parquet-types';
+import { ValueResult, ErrorsToResponse, SafeJSONParse } from 'rerrors';
+
+export interface Env {
+  LAKESIDE_BUCKET: R2Bucket;
+}
+
+const getSchema = async (env: Env): Promise<ValueResult<ParquetSchema>> => {
+  const schema = await env.LAKESIDE_BUCKET.get(`schema/schema.json`);
+  if (!schema) {
+    return {
+      success: false,
+      errors: ['No schema'],
+    };
+  }
+
+  const schemaText = await schema?.text();
+  const schemaJSON = SafeJSONParse(schemaText);
+  if (!schemaJSON.success) {
+    return {
+      success: false,
+      errors: ['Schema is not valid JSON'],
+    };
+  }
+
+  const parseResult = ParquetSchema.safeParse(schemaJSON.value);
+  if (parseResult.success) {
+    return {
+      success: true,
+      value: parseResult.data,
+    };
+  }
+
+  return {
+    success: false,
+    errors: [JSON.stringify(parseResult.error)],
+  };
+};
+
+export default {
+  async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+    const schemaResult = await getSchema(env);
+    if (!schemaResult.success) {
+      return ErrorsToResponse(schemaResult.errors);
+    }
+
+    try {
+      if (request.method === 'POST') {
+
+      }
+      return new Response('', { status: 405 });
+    } catch (e) {
+      return new Response('failed to process request', { status: 500 });
+    }
+  },
+};
