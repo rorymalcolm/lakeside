610acc0eb9854659b30118a3996e7fab373c7f1c
Rory Malcolm <rorymckenziemalcolm@gmail.com>
2024-01-27 15:08:46 +0000

    blahhh

--- compactor/src/worker.ts
+++ compactor/src/worker.ts
@@ -1,5 +1,6 @@
 import { ParquetSchema } from 'parquet-types';
 import { ValueResult, ErrorsToResponse, SafeJSONParse } from 'rerrors';
+import { generate_parquet as generateParquet } from 'parquet-generator';
 
 export interface Env {
   LAKESIDE_BUCKET: R2Bucket;
@@ -37,6 +38,52 @@ const getSchema = async (env: Env): Promise<ValueResult<ParquetSchema>> => {
   };
 };
 
+const getFiles = async (env: Env): Promise<ValueResult<string[]>> => {
+  const files = await env.LAKESIDE_BUCKET.list({ prefix: 'json/' });
+  if (!files) {
+    return {
+      success: false,
+      errors: ['No files'],
+    };
+  }
+
+  const errors: string[] = [];
+  const fileTexts = await Promise.all(files.objects.map(async (f) => {
+    const obj = await env.LAKESIDE_BUCKET.get(f.key)
+    const objText = await obj?.text();
+    if (!objText) {
+      errors.push(`No text for ${f.key}`);
+    }
+    return objText;
+  }));
+
+  if (errors.length > 0) {
+    return {
+      success: false,
+      errors,
+    };
+  }
+
+  const fileJSONs = (fileTexts as string[]).map(SafeJSONParse);
+  const fileJSONResults = await Promise.all(fileJSONs);
+  const fileJSONErrors = fileJSONResults.filter((r) => !r.success);
+  if (fileJSONErrors.length > 0) {
+    return {
+      success: false,
+      errors: fileJSONErrors.map((e) => !e.success ?
+        e.errors.map((e) => JSON.stringify(e)).join(', ') : ""
+      ).flat(),
+    };
+  }
+
+  const fileJSONValues = fileJSONResults.map((r) =>
+    r.success ? r.value : undefined).filter((v) => v !== undefined) as string[];
+  return {
+    success: true,
+    value: fileJSONValues,
+  }
+}
+
 export default {
   async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
     const schemaResult = await getSchema(env);
@@ -46,7 +93,16 @@ export default {
 
     try {
       if (request.method === 'POST') {
-
+        const filesResult = await getFiles(env);
+        if (!filesResult.success) {
+          return ErrorsToResponse(filesResult.errors);
+        }
+        const parquetFile = generateParquet(JSON.stringify(schemaResult.value), filesResult.value);
+        const putRes = await env.LAKESIDE_BUCKET.put('parquet/parquet.parquet', parquetFile);
+        if (!putRes) {
+          return new Response('', { status: 500 });
+        }
+        return new Response('OK');
       }
       return new Response('', { status: 405 });
     } catch (e) {
